3a510325e6d3cd395da3ae5f7efb097be31c24eb
Sergey Chernobrivenko <ser4@me.com>
2014-01-30 20:26:16 +0400

    post, get with parameters

--- src/ru/chernobrivenko/simpservl/ServletSample.java
+++ src/ru/chernobrivenko/simpservl/ServletSample.java
@@ -1,6 +1,9 @@
 package ru.chernobrivenko.simpservl;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,13 +18,29 @@ public class ServletSample extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter wr = response.getWriter();
+		response.setContentType("text/html");
+		wr.print("<h2>Test Get</h2>");
+		printParameterMap(request.getParameterMap(), wr);
 	}
 
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter wr = response.getWriter();
+		response.setContentType("text/html");
+		wr.print("<h2>Test Post</h2>");
+		printParameterMap(request.getParameterMap(), wr);
 	}
 
+
+	private void printParameterMap(Map<String, String[]> mp, PrintWriter wr)
+	{
+		for(Map.Entry<String, String[]> en:mp.entrySet())
+		{
+			wr.printf("<h3>Parameter %s:</h3>", en.getKey());
+			for(String s: en.getValue())
+				wr.printf("<p>%s</p>", s);
+		}
+	}
 }
