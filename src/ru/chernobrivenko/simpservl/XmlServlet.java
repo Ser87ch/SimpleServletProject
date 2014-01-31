5b6eabf989a15e9086eaa3ecd5f2ae41e21f7963
Sergey Chernobrivenko <ser4@me.com>
2014-01-31 11:03:25 +0400

    context params

--- src/ru/chernobrivenko/simpservl/XmlServlet.java
+++ src/ru/chernobrivenko/simpservl/XmlServlet.java
@@ -3,6 +3,7 @@ package ru.chernobrivenko.simpservl;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,13 +21,16 @@ public class XmlServlet extends HttpServlet {
 		wr.print("<h2>Test Get</h2>");
 		
 		HttpSession s = request.getSession();
-		String str = (String) s.getAttribute("p1s");
-		if(str == "" || str == null)
+		ServletContext sc = request.getServletContext();
+		
+		String str = request.getParameter("p1");
+		if(str != "" && str != null)
 		{
 			s.setAttribute("p1s", request.getParameter("p1"));
+			sc.setAttribute("p1c", request.getParameter("p1"));
 		}
 		
-		wr.printf("Session atribute: %s",  (String) s.getAttribute("p1s"));
-
+		wr.printf("Session atribute: %s <br>",  (String) s.getAttribute("p1s"));
+		wr.printf("Context atribute: %s",  (String) sc.getAttribute("p1c"));
 	}
 }
