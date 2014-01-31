bb6ffe872e7b6c244345aaf1437a7366da3f7cc6
Sergey Chernobrivenko <ser4@me.com>
2014-01-31 17:24:16 +0400

    print annotation param config

--- src/ru/chernobrivenko/simpservl/ServletSample.java
+++ src/ru/chernobrivenko/simpservl/ServletSample.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(description = "Simple servlet", urlPatterns = { "/ServletSample" }, 
+@WebServlet(description = "Simple servlet", urlPatterns = { "/ServletSample" },
 initParams={@WebInitParam(name="pi", value="test")})
 public class ServletSample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,7 +22,7 @@ public class ServletSample extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter wr = response.getWriter();
 		response.setContentType("text/html");
-		wr.print("<h2>Test Get</h2>");
+		wr.printf("<h2>Test Get %s</h2>", getServletConfig().getInitParameter("pi"));
 		printParameterMap(request.getParameterMap(), wr);
 	}
 
